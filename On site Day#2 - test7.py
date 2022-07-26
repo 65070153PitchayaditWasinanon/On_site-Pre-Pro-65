b9adade641a5e147bc43bd4d1b3f13ba53d8ec4f
65070153 Pitchayadit Wasinanon (นายพิชญดิฏฐ์ วศินานนท์) <109982392+65070153PitchayaditWasinanon@users.noreply.github.com>
2022-07-26 12:39:01 +0700

    Add files via upload

---
+++ On site Day#2 - test7.py
@@ -0,0 +1,11 @@
+"""[Onsite Day 2] เกมวิ่งเก็บ"""
+
+def main():
+    """DDD"""
+    txt = input()
+    pog = txt.split(" ")
+    total = []
+    for num in pog:
+        total.append(num)
+    
+main()
